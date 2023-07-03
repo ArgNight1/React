4aff5a939d707c72c01a2d6f763a9b7384797f6c
ArgNight <imperiale52@gmail.com>
2023-07-03 15:45:11 -0300

    PreEntrega1+Imperiale

---
+++ src/components/CartWidget.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { FcShop } from "react-icons/fc";
+//BsShop
+
+const CartWidget = () => {
+    return(
+        <div>
+            <h4><FcShop/>0</h4>
+        </div>
+    )
+}
+
+export default CartWidget
---
+++ src/components/ImgTemp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ImgTemp= () => {
+    return(
+        <div>
+            <img src='https://img.freepik.com/foto-gratis/ingeniero-civil-trabajador-construccion-arquitectos-que-usan-cascos-chalecos-seguridad-estan-trabajando-juntos-sitio-construccion-construyendo-casa-concepto-trabajo-equipo-cooperacion_640221-172.jpg?w=2000'  width="800" height="600" alt="trabajando... " />
+        </div>
+    )
+}
+
+export default ImgTemp
